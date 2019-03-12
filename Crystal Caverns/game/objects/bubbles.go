aeba2491bb5ed4af0964828dfdfca15e295f6d4a
Ben James <benjames171@gmail.com>
2019-03-12 11:17:13 +0000

    Crystal Caverns added

---
+++ Crystal Caverns/game/objects/bubbles.go
@@ -0,0 +1,30 @@
+components {
+  id: "pfx"
+  component: "/game/objects/bubbles.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "bubbles"
+  component: "/game/objects/bubbles.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
