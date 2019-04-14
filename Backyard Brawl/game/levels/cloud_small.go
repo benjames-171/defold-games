f8d2c57a41b104d321a092525e6b57258a3b417a
Ben James <benjames171@gmail.com>
2019-04-14 18:04:37 +0100

    update

---
+++ Backyard Brawl/game/levels/cloud_small.go
@@ -0,0 +1,35 @@
+components {
+  id: "cloud"
+  component: "/game/levels/cloud.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/main.atlas\"\n"
+  "default_animation: \"cloud_small\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
