5aefffc742a5614539380611ca1532e72fd16c91
Ben James <benjames171@gmail.com>
2021-04-26 10:32:14 +0100

    update

---
+++ Driller 2/main/data.lua
@@ -0,0 +1,71 @@
+local M = {}
+
+M.STATE_MENU = 1
+M.STATE_CONTROLS = 2
+M.STATE_GETREADY = 3
+M.STATE_PLAYING = 4
+M.STATE_LEVELCLEAR = 5
+M.STATE_GAMEOVER = 6
+
+M.state = M.STATE_MENU
+
+M.TILE_SIZE = 16
+
+M.level = 1
+M.maxlevel = 1
+M.score = 0
+M.best = 0
+M.starttime = 0
+M.smart = false
+M.endtime = 0
+M.mode = 2
+M.max = 0
+M.ext = 0
+M.oil = 0
+M.totoil = 0
+
+M.target = {60.99, 60.99, 70.99, 70.99, 70.99, 90.99, 80.99, 90.99, 100.99, 120.99}
+
+function M.hex2rgba(hex)
+	hex = hex:gsub("#","")
+	local rgba = vmath.vector4(tonumber("0x"..hex:sub(1,2)) / 255, tonumber("0x"..hex:sub(3,4)) / 255, tonumber("0x"..hex:sub(5,6)) / 255, 1)
+	return rgba
+end
+
+function M.world2tile(p)
+	return vmath.vector3(math.floor((p.x + M.TILE_SIZE) / M.TILE_SIZE), math.floor((p.y + M.TILE_SIZE) / M.TILE_SIZE), p.z)
+end
+
+function M.tile2world(p)
+	return vmath.vector3((p.x * M.TILE_SIZE) - (M.TILE_SIZE / 2), (p.y * M.TILE_SIZE) - (M.TILE_SIZE / 2), p.z)
+end
+
+function M.onscreen(p, m)
+	if p.x > M.scrollpos.x - m and
+	p.x < M.scrollpos.x + m + M.offset.x * 2 and
+	p.y > M.scrollpos.y - m and
+	p.y < M.scrollpos.y + m + M.offset.y * 2 then
+		return true
+	else
+		return false
+	end
+end
+
+function M.ms2str(time)
+	local day = math.floor(time / 86400)
+	local rem = time % 86400
+	local hr = math.floor(rem / 3600)
+	rem = rem % 3600
+	local min = math.floor(rem / 60)
+	rem = rem % 60
+	local sec = rem
+
+	local str = ""
+	if day > 0 then str = tostring(day) .. "d " end
+	if hr > 0 or day > 0 then str = str .. tostring(hr) .. ":" end
+
+	str = string.format("%s%02d:%02d", str, min, math.floor(sec))
+	return str
+end
+
+return M
